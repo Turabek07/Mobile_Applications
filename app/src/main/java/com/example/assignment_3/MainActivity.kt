e12ca80b0673f18fe98ade1d8874903f9dfaf4d1
Turabek07 <turabekyusubov587@gmail.com>
2024-12-09 22:13:02 +0500

    initial commit

--- app/src/main/java/com/example/assignment_3/MainActivity.kt
+++ app/src/main/java/com/example/assignment_3/MainActivity.kt
@@ -1,20 +1,17 @@
 package com.example.assignment_3
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+class MainActivity : AppCompatActivity()  {
+    /*Create new Account
+        Correct Input values
+            Password (8 charachteers/digits)
+            Email Format
+        Email already used - Error
+        Successful registration (new user is present in CredManager)
+        Case Insensitive in email
+
+        Apply it to SignUpDetailsActivity
+        After successful registration, go to CreateAccount activity
+        */
 }
