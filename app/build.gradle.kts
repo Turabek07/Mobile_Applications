5008470baac41a49b47c0ddadecf8ab89c219af8
Turabek07 <turabekyusubov587@gmail.com>
2024-12-12 09:18:34 +0500

    added fragment layouts and libraries also added version of Fragment  to gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.assignment_3"
-        minSdk = 24
+        minSdk = 22
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -40,13 +40,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
-    testImplementation(libs.junit.jupiter)
-    testImplementation(libs.junit.jupiter)
-    implementation("com.google.android.material:material:1.4.0")
-    implementation(libs.androidx.fragments)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
